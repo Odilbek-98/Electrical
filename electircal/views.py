06de670f20fbf758bc65950766682190ff67ac0d
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-08-06 08:57:41 +0500

    some changes fixed template

--- electircal/views.py
+++ electircal/views.py
@@ -7,3 +7,21 @@ def home_page_view(request):
 
 def about_page_view(request):
     return render(request=request,template_name="about.html")
+
+def service_page_view(request):
+    return render(request=request,template_name="service.html")
+
+def blog_page_view(request):
+    return render(request=request,template_name="blog.html")
+
+def project_page_view(request):
+    return render(request=request,template_name="project.html")
+
+def team_page_view(request):
+    return render(request=request,template_name="team.html")
+
+def testimonial_page_view(request):
+    return render(request=request,template_name="testimonial.html")
+
+def contact_page_view(request):
+    return render(request=request,template_name="contact.html")
